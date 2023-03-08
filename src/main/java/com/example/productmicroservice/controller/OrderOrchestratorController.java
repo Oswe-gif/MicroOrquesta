d373c2a4a84e18c8f7b025dd66f70a2dfa3c5928
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-03-08 15:01:54 -0500

    Agrega la clase orquestación

--- src/main/java/com/example/productmicroservice/controller/OrderOrchestratorController.java
+++ src/main/java/com/example/productmicroservice/controller/OrderOrchestratorController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class OrchestrationServiceApplication {
+public class OrderOrchestratorController {
     private ProductController productController;
     private UserController userController;
 
+    public String createOrder()
+    {
+
+        return "";
+    }
     @GetMapping("api/productOrder")
     public String processOrder()
     {
